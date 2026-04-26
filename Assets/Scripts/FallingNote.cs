ccb5fed92205b61eff5015dee9e861f2a7dd9979
Tyler Jeffrey <tyler_tj@icloud.com>
2026-04-25 21:11:02 -0700

    Created Target and Falling Arrows.

---
+++ Assets/Scripts/FallingNote.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FallingNote : MonoBehaviour
+{
+    [Header("Note Settings")]
+    public float fallSpeed = 5f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+    }
+}
