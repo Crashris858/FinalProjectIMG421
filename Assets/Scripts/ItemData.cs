1cfba4cf86aa86776e4c0510249ae82f73d5c376
Carson Shah <cas2343@nau.edu>
2026-04-28 18:59:44 -0700

    added world and created altered player and item logic

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -7,6 +7,16 @@ public class ItemData : MonoBehaviour
 {
     public string ItemName;
     public int ItemID; 
+    public float RespawnTime= 60f; 
+    private MeshRenderer meshRenderer=null; 
+    private SphereCollider sphereCollider=null;
+    public void Start()
+    {
+        //get components 
+        meshRenderer=GetComponentInChildren<MeshRenderer>();
+        sphereCollider=GetComponentInChildren<SphereCollider>();
+
+    }
     public void FixedUpdate()
     {
         //rotate 
@@ -15,7 +25,16 @@ public class ItemData : MonoBehaviour
 
     public void OnInteracted()
     {
-         Destroy(this.gameObject);
+         meshRenderer.enabled=false;
+         sphereCollider.enabled=false;
+         StartCoroutine(ReEnable());
+    }
+
+    public IEnumerator ReEnable()
+    {
+        yield return new WaitForSeconds(RespawnTime);
+        meshRenderer.enabled=true;
+        sphereCollider.enabled=true;
     }
 
 }
